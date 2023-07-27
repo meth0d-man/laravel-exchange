cf757e9815467205b188e543fe1fb61faf410993
Vladimir Ilic <vladimir.ilic@askgamblers.com>
2023-07-27 12:35:00 +0200

    add migrations set tables and model, seed db

---
+++ database/migrations/2023_07_26_103912_create_order_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order', function (Blueprint $table) {
+            $table->id();
+            $table->string('currency_name');
+            $table->decimal('exchange_rate');
+            $table->decimal('surcharge');
+            $table->string('foreign_currency_purchased');
+            $table->decimal('exchange_rate_for_foreign_currency');
+            $table->decimal('surcharge_percentage');
+            $table->decimal('amount_of_surcharge');
+            $table->decimal('amount_of_foreign_currency_purchased');
+            $table->decimal('amount_paid_in_USD');
+            $table->decimal('discount_percentage');
+            $table->decimal('discount_amount');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order');
+    }
+};
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Currency;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,6 +13,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        Currency::factory()->create([
+            'name' => 'Japanese Yen (JPY)',
+            'exchange_rate' => 107.17,
+            'surcharge' => 7.5
+        ]);
+        Currency::factory()->create([
+            'name' => 'British Pound (GBP)',
+            'exchange_rate' => 0.711178,
+            'surcharge' => 5
+        ]);
+        Currency::factory()->create([
+            'name' => 'Euro (EUR)',
+            'exchange_rate' => 0.884872,
+            'surcharge' => 5
+        ]);
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
