ebfafa527c84e2cb8be6d523d173093bfed76578
Vladimir Ilic <vladimir.ilic@askgamblers.com>
2023-08-01 11:41:42 +0200

    refactor migrations

--- database/migrations/2023_07_26_103210_create_currency_table.php
+++ database/migrations/2023_07_26_103210_create_currency_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->decimal('exchange_rate');
             $table->decimal('surcharge');
+            $table->string('discount');
             $table->timestamps();
         });
     }
--- database/migrations/2023_07_26_103912_create_order_table.php
+++ database/migrations/2023_07_26_103912_create_order_table.php
@@ -15,15 +15,12 @@ return new class extends Migration
             $table->id();
             $table->string('currency_name');
             $table->decimal('exchange_rate');
-            $table->decimal('surcharge');
             $table->string('foreign_currency_purchased');
             $table->decimal('exchange_rate_for_foreign_currency');
             $table->decimal('surcharge_percentage');
-            $table->decimal('amount_of_surcharge');
             $table->decimal('amount_of_foreign_currency_purchased');
             $table->decimal('amount_paid_in_USD');
             $table->decimal('discount_percentage');
-            $table->decimal('discount_amount');
             $table->timestamps();
 
         });
